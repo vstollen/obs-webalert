740b1fc48da7a3692b1ebfe0eac0d64f8f9362cc
vstollen <vstollen@gmail.com>
2020-11-07 12:55:33 +0100

    Initial Commit
    Add basic SSE implementation

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"webalert/sse"
+)
+
+var templates = template.Must(template.ParseFiles("tmpl/feed.html"))
+
+func feedHandler(w http.ResponseWriter, _ *http.Request) {
+	err := templates.ExecuteTemplate(w, "feed.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func cmdMessenger(m chan string) {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		message, err := reader.ReadString('\n')
+
+		if err != nil {
+			log.Println("An Error occured while reading your input:")
+			log.Println(err.Error())
+		}
+
+		m <- message
+	}
+}
+
+func main() {
+
+	messages := make(chan string)
+	broker := &sse.Broker{
+		Messages: messages,
+	}
+
+	http.HandleFunc("/feed/", feedHandler)
+	http.Handle("/events", broker)
+
+	go broker.ServeMessages()
+	go cmdMessenger(messages)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ sse/sse.go
@@ -0,0 +1,85 @@
+package sse
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Broker struct {
+	// Channel into which messages are pushed to be broadcast out to attached clients.
+	Messages chan string
+
+	// Set of clients, that should be messaged with incoming Messages.
+	// The Set is represented by a map of empty structs. To add entries do:
+	//   clients[chan] = struct{}{}
+	clients map[client]struct{}
+}
+
+type client chan string
+
+func (b *Broker) ServeMessages() {
+	for {
+		msg := <-b.Messages
+
+		for client := range b.clients {
+			client <- msg
+		}
+		log.Printf("Broadcast message to %d clients", len(b.clients))
+	}
+}
+
+func (b *Broker) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
+		return
+	}
+
+	client := b.registerClient()
+
+	go func() {
+		<- r.Context().Done()
+		b.removeClient(client)
+	}()
+
+	// Event Streaming Header
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Transfer-Encoding", "chunked")
+
+	for {
+		msg, open := <-client
+
+		if !open {
+			break
+		}
+
+		_, err := fmt.Fprintf(w, "data: Message: %s\n\n", msg)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		flusher.Flush()
+	}
+}
+
+func (b *Broker) registerClient() client {
+	if b.clients == nil {
+		b.clients = make(map[client]struct{})
+	}
+
+	client := make(client)
+
+	b.clients[client] = struct{}{}
+
+	return client
+}
+
+func (b *Broker) removeClient(client client) {
+	delete(b.clients, client)
+	close(client)
+}
